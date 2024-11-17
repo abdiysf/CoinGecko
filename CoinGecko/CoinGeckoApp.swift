a9e2db8e4e04cc34af0e45890dc7ad0dab3e7f82
abdiysf <mustafeabdisalam@gmail.com>
2024-11-17 22:52:20 +0300

    Initial Commit

---
+++ CoinGecko/CoinGeckoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinGeckoApp.swift
+//  CoinGecko
+//
+//  Created by Abdirahman Standard on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinGeckoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
